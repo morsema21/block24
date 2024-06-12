14ccb3cca49706572d067bcd457218c4bd3cac2f
Molly Morse <mollymorse@Mollys-MacBook-Air.local>
2024-06-12 10:36:13 -0500

    section 2

--- block24/src/App.jsx
+++ block24/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
-
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
